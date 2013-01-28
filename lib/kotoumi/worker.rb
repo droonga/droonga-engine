28da65f5b63b189040cce95cb2d0d93bd1172ecb
Kouhei Sutou <kou@clear-code.com>
2013-01-28 12:35:50 +0900

    Indent

--- lib/kotoumi/worker.rb
+++ lib/kotoumi/worker.rb
@@ -17,27 +17,27 @@
 
 require 'groonga'
 
-  module Kotoumi
-    class Worker
-      def initialize(database, queuename)
-        Groonga::Database.open(database)
-        @ctx = Groonga::Context.default
-        @queuename = queuename
-      end
+module Kotoumi
+  class Worker
+    def initialize(database, queuename)
+      Groonga::Database.open(database)
+      @ctx = Groonga::Context.default
+      @queuename = queuename
+    end
 
-      def process_message(tag, time, record)
-        return {
-          "main-search-result" => {
-            startTime: "2001-08-02T10:45:23.5+09:00",
-            elapsedTime: 123.456,
-            count: 123,
-            attributes: [
-              { name: "name", type: "ShortText", vector: false },
-              { name: "age", type: "UInt32", vector: false }
-            ],
-            records: [ ["a", 10], ["b", 20] ]
-          }
+    def process_message(tag, time, record)
+      return {
+        "main-search-result" => {
+          startTime: "2001-08-02T10:45:23.5+09:00",
+          elapsedTime: 123.456,
+          count: 123,
+          attributes: [
+            { name: "name", type: "ShortText", vector: false },
+            { name: "age", type: "UInt32", vector: false }
+          ],
+          records: [ ["a", 10], ["b", 20] ]
         }
-      end
+      }
     end
   end
+end
