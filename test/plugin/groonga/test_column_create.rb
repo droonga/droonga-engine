f9f5e198f7b2b515061534974455d91997d9785f
Kosuke Asami <tfortress58@gmail.com>
2013-07-22 15:07:01 +0900

    test column_create: define an expected dumped schema as a here document

--- test/plugin/groonga/test_column_create.rb
+++ test/plugin/groonga/test_column_create.rb
@@ -89,7 +89,12 @@ column_create Books entry_title COLUMN_INDEX Books title
           "flags"  => data[:flags],
         }
         @handler.column_create(request)
-        assert_equal("table_create Books TABLE_HASH_KEY --key_type ShortText\ncolumn_create Books title COLUMN_SCALAR ShortText\n\n#{data[:schema]}", dump)
+        assert_equal(<<-EXPECTED.chomp, dump)
+table_create Books TABLE_HASH_KEY --key_type ShortText
+column_create Books title COLUMN_SCALAR ShortText
+
+#{data[:schema]}
+        EXPECTED
       end
 
       data({
