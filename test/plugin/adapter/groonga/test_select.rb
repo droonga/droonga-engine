cd29069d25ba1e5cafc9839e234fac2523e5cccd
Kosuke Asami <tfortress58@gmail.com>
2013-07-23 14:43:48 +0900

    Move adapter files related to groonga-like API to "groonga" directory
    plugin/adapter/adapter_select.rb ->
    plugin/adapter/groonga/select.rb

--- test/plugin/adapter/groonga/test_select.rb
+++ test/plugin/adapter/groonga/test_select.rb
@@ -13,7 +13,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require "droonga/plugin/adapter/adapter_select"
+require "droonga/plugin/adapter/groonga/select"
 
 class AdapterGroongaSelectTest < Test::Unit::TestCase
   def setup
