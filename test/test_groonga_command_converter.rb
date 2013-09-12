6c0b6c9c135c4e45f9518cbd99601bd1db2f5b47
YUKI Hiroshi <shimoda@clear-code.com>
2013-09-12 14:23:09 +0900

    Load tested module correctly

--- test/test_groonga_command_converter.rb
+++ test/test_groonga_command_converter.rb
@@ -13,7 +13,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require "droonga/groonga_command_converter"
+require "groonga_command_converter"
 
 class GroongaCommandConverterTest < Test::Unit::TestCase
   def setup
