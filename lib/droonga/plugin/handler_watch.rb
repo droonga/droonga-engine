8a73d76176cfeeaf460dee4ff2ca799f9d7358db
Yoji Shidara <dara@shidara.net>
2013-10-11 14:51:20 +0900

    Implement watch method

--- lib/droonga/plugin/handler_watch.rb
+++ lib/droonga/plugin/handler_watch.rb
@@ -23,8 +23,27 @@ module Droonga
 
     command "watch"
     def watch(request)
-      p parse_request(request)
-      # TODO
+      user, condition, query, route = parse_request(request)
+      query_table = @context['Query']
+      query_record = query_table[query]
+      unless query_record
+        keywords = pick_keywords([], condition)
+        query_record = query_table.add(query, :keywords => keywords)
+      end
+      user_table = @context['User']
+      user_record = user_table[user]
+      if user_record
+        subscriptions = user_record.subscriptions.collect do |query|
+          return if query == query_record
+          query
+        end
+        subscriptions << query_record
+        user_record.subscriptions = subscriptions
+      else
+        user_table.add(user,
+                       :subscriptions => [query_record],
+                       :route => route)
+      end
     end
 
     private
@@ -36,6 +55,20 @@ module Droonga
       query = condition.to_json
       raise "too long query" if query.size > 4095
       [user, condition, query, route]
+    end
+
+    def pick_keywords(memo, condition)
+      case condition
+      when Hash
+        memo << condition["query"]
+      when String
+        memo << condition
+      when Array
+        condition[1..-1].each do |element|
+          pick_keywords(memo, element)
+        end
+      end
+      memo
     end
   end
 end
