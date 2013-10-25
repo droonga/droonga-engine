fe35a88cd2c73c60329449929e8bf9d030840f4a
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-25 18:53:59 +0900

    Benchmark: remove needless class

--- benchmark/utils.rb
+++ benchmark/utils.rb
@@ -16,13 +16,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 module DroongaBenchmark
-  class StubWorker
-    attr_reader :context
-    def initialize(context)
-      @context = context
-    end
-  end
-
   class WatchDatabase
     attr_reader :context
 
