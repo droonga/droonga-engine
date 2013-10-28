7fb7584f9477c265bb5d220cb6b8e6fb54347556
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-28 11:09:04 +0900

    Fix indent

--- benchmark/watch/benchmark-scan.rb
+++ benchmark/watch/benchmark-scan.rb
@@ -72,16 +72,15 @@ n_watching_terms = 1000
 step             = 1000
 n_tests          = 20
 incidences       = [0.1, 0.5, 0.9]
-  puts "starting..."
-  incidences.each do |incidence|
-    scan_benchmark = ScanBenchmark.new(n_watching_terms, incidence)
-    n_tests.times do |try_count|
-Benchmark.bmbm do |benchmark|
+
+incidences.each do |incidence|
+  scan_benchmark = ScanBenchmark.new(n_watching_terms, incidence)
+  n_tests.times do |try_count|
+    Benchmark.bmbm do |benchmark|
       scan_benchmark.add_terms(step) if try_count > 0
       benchmark.report("incidence #{incidence}, #{scan_benchmark.n_terms} keywords") do
         scan_benchmark.run
       end
-end
     end
   end
-
+end
