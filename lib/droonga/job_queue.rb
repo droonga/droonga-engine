7a1b849d068c90f440d38e5e4578693c036b6383
Yoji Shidara <dara@shidara.net>
2013-10-31 17:26:10 +0900

    Extract job_queue operations into JobQueue class

--- lib/droonga/job_queue.rb
+++ lib/droonga/job_queue.rb
@@ -16,6 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 require "droonga/job_queue_schema"
+require "msgpack"
 
 module Droonga
   class JobQueue
@@ -24,6 +25,59 @@ module Droonga
         schema = JobQueueSchema.new(database_path, queue_name)
         schema.ensure_created
       end
+
+      def open(database_path, queue_name)
+        job_queue = new(database_path, queue_name)
+        job_queue.open
+        job_queue
+      end
+    end
+
+    def initialize(database_path, queue_name)
+      @database_path = database_path
+      @queue_name = queue_name
+    end
+
+    def open
+      @context = Groonga::Context.new
+      @database = @context.open_database(@database_path)
+      @context.encoding = :none
+
+      @queue = @context[@queue_name]
+    end
+
+    def push_message(message)
+      $log.trace("#{log_tag}: push_message: start")
+      packed_message = message.to_msgpack
+      @queue.push do |record|
+        record.message = packed_message
+      end
+      $log.trace("#{log_tag}: push_message: done")
+    end
+
+    def pull_message
+      packed_message = nil
+      @queue.pull do |record|
+        if record
+          packed_message = record.message
+          record.delete
+        end
+      end
+      return nil unless packed_message
+      MessagePack.unpack(packed_message)
+    end
+
+    def close
+      @queue = nil
+      if @database
+        @database.close
+        @context.close
+        @database = @context = nil
+      end
+    end
+
+    def log_tag
+      "[#{Process.ppid}][#{Process.pid}] job_queue"
     end
   end
 end
