f3da2f3971ba9baa116c3a0576df9118dabf8e6f
Kouhei Sutou <kou@clear-code.com>
2013-12-03 18:29:49 +0900

    Follow command line option name change in drntest

--- test/command/run-test.rb
+++ test/command/run-test.rb
@@ -29,7 +29,7 @@ lib_dir = File.expand_path(File.join(base_dir, "..", "..", "lib"))
 
 drntest_options = []
 drntest_options.concat(["--fluentd-options", "-I#{lib_dir}"])
-drntest_options.concat(["--base", base_dir])
+drntest_options.concat(["--base-path", base_dir])
 drntest_options.concat(ARGV)
 
 run("bundle", "exec", "drntest", *drntest_options)
