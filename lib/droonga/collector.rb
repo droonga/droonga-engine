9492b0aa2c5682a61fa65109c7b3d8bc5e5366f5
Kouhei Sutou <kou@clear-code.com>
2013-12-20 15:58:29 +0900

    Remove unused codes

--- lib/droonga/collector.rb
+++ lib/droonga/collector.rb
@@ -71,30 +71,14 @@ module Droonga
         type = component["type"]
         command = "collector_" + type
         n_of_expects = component["n_of_expects"]
-        synchronous = nil
-        synchronous = true unless n_of_expects.zero?
-        # TODO: check if asynchronous execution is available.
         message = {
           "task"=>task,
           "name"=>name,
           "value"=>value
         }
-        unless synchronous
-          descendants = {}
-          component["descendants"].each do |name, indices|
-            descendants[name] = indices.collect do |index|
-              @components[index]["routes"].map do |route|
-                @dispatcher.farm_path(route)
-              end
-            end
-          end
-          message["descendants"] = descendants
-          message["id"] = @id
-        end
         process(command, message)
         return if task["n_of_inputs"] < n_of_expects
         #the task is done
-        if synchronous
           result = task["values"]
           post = component["post"]
           @dispatcher.post(result, post) if post
@@ -110,7 +94,6 @@ module Droonga
               end
             end
           end
-        end
         @n_dones += 1
         @dispatcher.collectors.delete(@id) if @n_dones == @tasks.size
       end
