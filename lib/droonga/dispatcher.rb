350932c02d22491ad025c9a16b1e51278b587543
Kouhei Sutou <kou@clear-code.com>
2013-12-24 16:51:16 +0900

    Use " (double quote) for string literal

--- lib/droonga/dispatcher.rb
+++ lib/droonga/dispatcher.rb
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 require "English"
-require 'tsort'
+require "tsort"
 
 require "droonga/input_adapter"
 require "droonga/output_adapter"
