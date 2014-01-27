db59cefb7c036f0116424cf61dda386135fcb945
YUKI Hiroshi <shimoda@clear-code.com>
2014-01-27 12:00:28 +0900

    Reduce indent

--- lib/droonga/plugin/collector/basic.rb
+++ lib/droonga/plugin/collector/basic.rb
@@ -43,27 +43,27 @@ module Droonga
     end
 
     def reduce(deal, left_value, right_value)
-        reduced_value = nil
+      reduced_value = nil
 
-        case deal["type"]
-        when "and"
-          reduced_value = left_value && right_value
-        when "sum"
-          reduced_value = left_value + right_value
-          reduced_value = apply_output_range(reduced_value,
-                                             "limit" => deal["limit"])
-        when "average"
-          reduced_value = (left_value + right_value) / 2
-        when "sort"
-          reduced_value = merge(left_value,
-                                right_value,
-                                :operators => deal["operators"],
-                                :key_column => deal["key_column"])
-          reduced_value = apply_output_range(reduced_value,
-                                             "limit" => deal["limit"])
-        end
+      case deal["type"]
+      when "and"
+        reduced_value = left_value && right_value
+      when "sum"
+        reduced_value = left_value + right_value
+        reduced_value = apply_output_range(reduced_value,
+                                           "limit" => deal["limit"])
+      when "average"
+        reduced_value = (left_value + right_value) / 2
+      when "sort"
+        reduced_value = merge(left_value,
+                              right_value,
+                              :operators => deal["operators"],
+                              :key_column => deal["key_column"])
+        reduced_value = apply_output_range(reduced_value,
+                                           "limit" => deal["limit"])
+      end
 
-        reduced_value
+      reduced_value
     end
 
     def apply_output_range(items, output)
