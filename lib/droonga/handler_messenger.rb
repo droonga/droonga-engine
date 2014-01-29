df0695c4319c142871a193ae09236319b058d733
YUKI Hiroshi <shimoda@clear-code.com>
2014-01-29 17:02:22 +0900

    Update copyright year

--- lib/droonga/handler_messenger.rb
+++ lib/droonga/handler_messenger.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 Droonga Project
+# Copyright (C) 2013-2014 Droonga Project
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
