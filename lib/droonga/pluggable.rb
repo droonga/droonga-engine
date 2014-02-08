bff0e4adef536583efd5e241c07de80b8be60fc7
Kouhei Sutou <kou@clear-code.com>
2014-02-08 13:50:51 +0900

    Remove unused code

--- lib/droonga/pluggable.rb
+++ lib/droonga/pluggable.rb
@@ -13,14 +13,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require "droonga/plugin/metadata/plugin"
-
 module Droonga
   module Pluggable
-    def plugin
-      Plugin::Metadata::Plugin.new(self)
-    end
-
     def options
       @options ||= {}
     end
