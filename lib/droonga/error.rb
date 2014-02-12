fb0f05921f308cd2096fab92bc9a2cfef9cf095a
YUKI Hiroshi <shimoda@clear-code.com>
2014-02-12 13:27:15 +0900

    Define base error class for errors in Droonga

--- lib/droonga/error.rb
+++ lib/droonga/error.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2013-2014 Droonga Project
+# Copyright (C) 2014 Droonga Project
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,14 +15,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require "droonga/catalog_loader"
-require "droonga/error"
-
 module Droonga
-  class << self
-    attr_accessor :catalog
-  end
-
-  class InvalidCatalog < Error
+  class Error < StandardError
   end
 end
--- lib/droonga/message_processing_error.rb
+++ lib/droonga/message_processing_error.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 Droonga Project
+# Copyright (C) 2013-2014 Droonga Project
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -13,8 +13,10 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+require "droonga/error"
+
 module Droonga
-  class MessageProcessingError < StandardError
+  class MessageProcessingError < Error
     STATUS_CODE = 500
 
     attr_reader :message, :detail
