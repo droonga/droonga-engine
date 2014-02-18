7e17eafcd6586ed7ac267e409eaa76b3acf3e165
Kouhei Sutou <kou@clear-code.com>
2014-02-18 14:20:06 +0900

    Align

--- lib/droonga/status_code.rb
+++ lib/droonga/status_code.rb
@@ -17,9 +17,9 @@
 
 module Droonga
   module StatusCode
-    OK             = 200
-    BAD_REQUEST    = 400
-    NOT_FOUND      = 404
+    OK                    = 200
+    BAD_REQUEST           = 400
+    NOT_FOUND             = 404
     INTERNAL_SERVER_ERROR = 500
   end
 end
