693d18c8fc7cbd5d89ecdaa7fb5fdf9047457b9a
Kouhei Sutou <kou@clear-code.com>
2014-03-17 18:25:05 +0900

    Don't write to socket from non loop.run thread

--- lib/droonga/fluent_message_sender.rb
+++ lib/droonga/fluent_message_sender.rb
@@ -15,6 +15,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+require "thread"
+
 require "cool.io"
 
 require "droonga/loggable"
@@ -28,32 +30,40 @@ module Droonga
       @loop = loop
       @host = host
       @port = port
-      @connected = false
+      @socket = nil
+      @buffer = []
+      @write_mutex = Mutex.new
     end
 
     def start
       logger.trace("start: start")
-      connect
+      start_writer
       logger.trace("start: done")
     end
 
     def shutdown
       logger.trace("shutdown: start")
-      @socket.close unless @socket.closed?
+      shutdown_writer
+      shutdown_socket
       logger.trace("shutdown: done")
     end
 
     def send(tag, data)
       logger.trace("send: start")
-      connect unless @connected
       fluent_message = [tag, Time.now.to_i, data]
       packed_fluent_message = MessagePackPacker.pack(fluent_message)
-      @socket.write(packed_fluent_message)
-      @loop.break_current_loop
+      @write_mutex.synchronize do
+        @buffer << packed_fluent_message
+        @writer.signal
+      end
       logger.trace("send: done")
     end
 
     private
+    def connected?
+      not @socket.nil?
+    end
+
     def connect
       logger.trace("connect: start")
 
@@ -62,16 +72,15 @@ module Droonga
       end
       log_connect = lambda do
         logger.trace("connected to #{@host}:#{@port}")
-        @connected = true
       end
       log_failed = lambda do
         logger.error("failed to connect to #{@host}:#{@port}")
+        @socket = nil
       end
       on_close = lambda do
-        @connected = false
+        @socket = nil
       end
 
-      @connected = false
       @socket = Coolio::TCPSocket.connect(@host, @port)
       @socket.on_write_complete do
         log_write_complete.call
@@ -88,6 +97,34 @@ module Droonga
       @loop.attach(@socket)
 
       logger.trace("connect: done")
+    end
+
+    def shutdown_socket
+      return unless connected?
+      @socket.close unless @socket.closed?
+    end
+
+    def start_writer
+      @writer = Coolio::AsyncWatcher.new
+
+      on_signal = lambda do
+        @write_mutex.synchronize do
+          connect unless connected?
+          @buffer.each do |data|
+            @socket.write(data)
+          end
+          @buffer.clear
+        end
+      end
+      @writer.on_signal do
+        on_signal.call
+      end
+
+      @loop.attach(@writer)
+    end
+
+    def shutdown_writer
+      @writer.detach
     end
 
     def log_tag
