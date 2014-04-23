42dfd41210df0d8672ac22bbca133a54a219ea44
YUKI Hiroshi <shimoda@clear-code.com>
2014-04-23 14:17:05 +0900

    test: Add tests for column_rename command

---
+++ test/unit/plugins/groonga/test_column_rename.rb
@@ -0,0 +1,104 @@
+# Copyright (C) 2014 Droonga Project
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+class ColumnRenameTest < GroongaHandlerTest
+  def create_handler
+    Droonga::Plugins::Groonga::ColumnRename::Handler.new("droonga",
+                                                         @handler.context,
+                                                         @messages,
+                                                         @loop)
+  end
+
+  def test_success
+    Groonga::Schema.define(:context => @context) do |schema|
+      schema.create_table("Books", :type => :hash)
+      schema.change_table("Books") do |table|
+        table.column("title", "ShortText", :type => :scalar)
+      end
+    end
+    message = {
+      "table"    => "Books",
+      "name"     => "title",
+      "new_name" => "label",
+    }
+    response = process(:column_rename, message)
+    assert_equal(
+      [NORMALIZED_HEADER_SUCCESS, true],
+      [normalize_header(response.first), response.last]
+    )
+  end
+
+  def test_unknown_table
+    message = {
+      "table"    => "Unknown",
+      "name"     => "title",
+      "new_name" => "label",
+      "type"     => "ShortText",
+    }
+    response = process(:column_rename, message)
+    assert_equal(
+      [NORMALIZED_HEADER_INVALID_ARGUMENT, false],
+      [normalize_header(response.first), response.last]
+    )
+  end
+
+  def test_unknown_column
+    Groonga::Schema.define(:context => @context) do |schema|
+      schema.create_table("Books", :type => :hash)
+    end
+    message = {
+      "table"    => "Books",
+      "name"     => "title",
+      "new_name" => "label",
+    }
+    response = process(:column_rename, message)
+    assert_equal(
+      [NORMALIZED_HEADER_INVALID_ARGUMENT, false],
+      [normalize_header(response.first), response.last]
+    )
+  end
+
+  def test_rename
+    Groonga::Schema.define(:context => @context) do |schema|
+      schema.create_table("Books", :type => :hash)
+      schema.change_table("Books") do |table|
+        table.column("title", "ShortText", :type => :scalar)
+      end
+    end
+    process(:column_rename,
+            {"table" => "Books", "name" => "title", "new_name" => "label"})
+    assert_equal(<<-SCHEMA, dump)
+table_create Books TABLE_HASH_KEY --key_type ShortText
+column_create Books label COLUMN_SCALAR ShortText
+    SCHEMA
+  end
+
+  def test_rename_with_index
+    Groonga::Schema.define(:context => @context) do |schema|
+      schema.create_table("Books", :type => :hash)
+      schema.change_table("Books") do |table|
+        table.column("title", "ShortText", :type => :scalar)
+        table.index("Books", "title", :name => "entry_title")
+      end
+    end
+    process(:column_rename,
+            {"table" => "Books", "name" => "title", "new_name" => "label"})
+    assert_equal(<<-SCHEMA, dump)
+table_create Books TABLE_HASH_KEY --key_type ShortText
+column_create Books label COLUMN_SCALAR ShortText
+column_create Books entry_title COLUMN_INDEX Books label
+    SCHEMA
+  end
+end
