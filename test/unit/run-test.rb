fb23cf1dc1105a8c3a796260108a1478d5069303
Kouhei Sutou <kou@clear-code.com>
2014-04-24 15:49:49 +0900

    Enable warnings on test
    Oh... We have many warnings...

--- test/unit/run-test.rb
+++ test/unit/run-test.rb
@@ -32,6 +32,10 @@ require "test-unit"
 require "test/unit/notify"
 require "test/unit/rr"
 
+require "cool.io"
+
+$VERBOSE = true
+
 base_dir = File.expand_path(File.join(File.dirname(__FILE__), "..", ".."))
 lib_dir = File.join(base_dir, "lib")
 test_dir = File.join(base_dir, "test", "unit")
