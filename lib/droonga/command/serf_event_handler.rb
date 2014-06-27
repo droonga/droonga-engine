73b35901c56c151739a975d824da7f613ef1842f
YUKI Hiroshi <shimoda@clear-code.com>
2014-06-28 02:34:53 +0900

    Remove needless require

--- lib/droonga/command/serf_event_handler.rb
+++ lib/droonga/command/serf_event_handler.rb
@@ -13,11 +13,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require "optparse"
-require "pathname"
 require "json"
-require "fileutils"
-require "tempfile"
 
 require "droonga/path"
 require "droonga/serf"
