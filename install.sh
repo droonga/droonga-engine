9a760cb173066c74347e7968782ca1b0b4415939
long-long-float <niinikazuki@yahoo.co.jp>
2014-09-01 18:04:43 +0900

    Make dir as droonga-engine user

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
-DROONGA_BASE_DIR = $(HOME)/droonga
-
+# install droonga
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y ruby ruby-dev build-essential
 sudo gem install droonga-engine
-mkdir $(DROONGA_BASE_DIR)
-cd $(DROONGA_BASE_DIR)
+
+# add droonga-engine user and create files
+adduser droonga-engine
+
+login droonga-engine
+DROONGA_BASE_DIR = /home/droonga-engine/droonga
+mkdir $DROONGA_BASE_DIR
+cd $DROONGA_BASE_DIR
 droonga-engine-catalog-generate --output=./catalog.json
 echo "host: " > droonga-engine.yaml
-
+exit
