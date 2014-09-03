e88eff7044e3bf8ccf695cc702b17b0967f6b9de
long-long-float <niinikazuki@yahoo.co.jp>
2014-09-04 01:36:09 +0900

    Collect constant variables for imploving maintainability

--- script/install.sh
+++ script/install.sh
@@ -8,19 +8,19 @@ apt-get -y upgrade
 apt-get install -y ruby ruby-dev build-essential
 gem install droonga-engine
 
+SCRIPT_URL=https://raw.githubusercontent.com/droonga/droonga-engine/master/script
+USER=droonga-engine
+DROONGA_BASE_DIR=/home/$USER/droonga
+
 exist_user() {
   grep "^$1:" /etc/passwd > /dev/null
 }
 
 # fetch files
-SCRIPT_URL=https://raw.githubusercontent.com/droonga/droonga-engine/master/script
 curl -O $SCRIPT_URL/droonga-engine -O $SCRIPT_URL/droonga-engine.yaml
 
 # add droonga-engine user and create files
-USER=droonga-engine
 exist_user $USER || useradd -m $USER
-
-DROONGA_BASE_DIR=/home/$USER/droonga
 droonga-engine-catalog-generate --output=./catalog.json
 mkdir $DROONGA_BASE_DIR
 mv catalog.json droonga-engine.yaml $DROONGA_BASE_DIR
