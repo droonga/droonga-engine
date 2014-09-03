f4a73a7ccc29cfcf13398ca9bcfff44187fab93c
long-long-float <niinikazuki@yahoo.co.jp>
2014-09-03 15:59:46 +0900

    Remove sudo prefix

--- script/install.sh
+++ script/install.sh
@@ -1,8 +1,8 @@
 # install droonga
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install -y ruby ruby-dev build-essential
-sudo gem install droonga-engine
+apt-get update
+apt-get -y upgrade
+apt-get install -y ruby ruby-dev build-essential
+gem install droonga-engine
 
 # fetch files
 #SCRIPT_URL=https://raw.github.com/droonga/droonga-engine/script
@@ -10,14 +10,14 @@ sudo gem install droonga-engine
 
 # add droonga-engine user and create files
 USER=droonga-engine
-sudo useradd -m $USER
+useradd -m $USER
 
 DROONGA_BASE_DIR=/home/$USER/droonga
 droonga-engine-catalog-generate --output=./catalog.json
-sudo mkdir $DROONGA_BASE_DIR
-sudo mv catalog.json droonga-engine.yaml $DROONGA_BASE_DIR
-sudo chown -R $USER.$USER $DROONGA_BASE_DIR
+mkdir $DROONGA_BASE_DIR
+mv catalog.json droonga-engine.yaml $DROONGA_BASE_DIR
+chown -R $USER.$USER $DROONGA_BASE_DIR
 
 # set up service
-sudo mv droonga-engine /etc/init.d/droonga-engine
-sudo update-rc.d droonga-engine defaults
+mv droonga-engine /etc/init.d/droonga-engine
+update-rc.d droonga-engine defaults
