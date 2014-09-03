92ddb09ffedb18d8b1316a459a51b6b6741d05b6
long-long-float <niinikazuki@yahoo.co.jp>
2014-09-04 01:40:28 +0900

    Download files directly

--- script/install.sh
+++ script/install.sh
@@ -16,16 +16,15 @@ exist_user() {
   grep "^$1:" /etc/passwd > /dev/null
 }
 
-# fetch files
-curl -O $SCRIPT_URL/droonga-engine -O $SCRIPT_URL/droonga-engine.yaml
-
 # add droonga-engine user and create files
 exist_user $USER || useradd -m $USER
+
 droonga-engine-catalog-generate --output=./catalog.json
 mkdir $DROONGA_BASE_DIR
-mv catalog.json droonga-engine.yaml $DROONGA_BASE_DIR
+curl -o $DROONGA_BASE_DIR/droonga-engine.yaml $SCRIPT_URL/droonga-engine.yaml
+mv catalog.json $DROONGA_BASE_DIR
 chown -R $USER.$USER $DROONGA_BASE_DIR
 
 # set up service
-mv droonga-engine /etc/init.d/droonga-engine
+curl -o /etc/init.d/droonga-engine $SCRIPT_URL/droonga-engine
 update-rc.d droonga-engine defaults
