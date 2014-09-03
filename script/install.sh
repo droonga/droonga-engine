87e9564aed17f13b6a6ceddddfceb35f27a7e8a6
long-long-float <niinikazuki@yahoo.co.jp>
2014-09-04 02:43:49 +0900

    Fixed usage of [

--- script/install.sh
+++ script/install.sh
@@ -6,7 +6,7 @@
 apt-get update
 apt-get -y upgrade
 apt-get install -y ruby ruby-dev build-essential
-gem install droonga-engine
+gem install droonga-engine --no-rdoc --no-ri
 
 SCRIPT_URL=https://raw.githubusercontent.com/droonga/droonga-engine/master/script
 USER=droonga-engine
@@ -19,15 +19,15 @@ exist_user() {
 # add droonga-engine user and create files
 exist_user $USER || useradd -m $USER
 
-[ ! $DROONGA_BASE_DIR ] &&
+[ ! -e $DROONGA_BASE_DIR ] &&
   mkdir $DROONGA_BASE_DIR
-[ ! $DROONGA_BASE_DIR/catalog.json ] &&
+[ ! -e $DROONGA_BASE_DIR/catalog.json ] &&
   droonga-engine-catalog-generate --output=$DROONGA_BASE_DIR/catalog.json
-[ ! $DROONGA_BASE_DIR/droonga-engine.yaml ] &&
+[ ! -e $DROONGA_BASE_DIR/droonga-engine.yaml ] &&
   curl -o $DROONGA_BASE_DIR/droonga-engine.yaml $SCRIPT_URL/droonga-engine.yaml
 chown -R $USER.$USER $DROONGA_BASE_DIR
 
 # set up service
-[ ! /etc/init.d/droonga-engine ] &&
+[ ! -e /etc/init.d/droonga-engine ] &&
   curl -o /etc/init.d/droonga-engine $SCRIPT_URL/droonga-engine
 update-rc.d droonga-engine defaults
