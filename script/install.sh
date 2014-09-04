701f6e2e85f208b0d0c6339d4af1d468b037dc39
YUKI Hiroshi <shimoda@clear-code.com>
2014-09-04 18:38:45 +0900

    Add license header

--- script/install.sh
+++ script/install.sh
@@ -1,3 +1,18 @@
+# Copyright (C) 2014 Droonga Project
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 SCRIPT_URL=https://raw.githubusercontent.com/droonga/droonga-engine/master/script
 USER=droonga-engine
 DROONGA_BASE_DIR=/home/$USER/droonga
