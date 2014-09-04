615a3bb2d03657dbd14bb643047e757cc73936de
long-long-float <niinikazuki@yahoo.co.jp>
2014-09-04 17:35:23 +0900

    Add install script for CentOS

--- script/install.sh
+++ script/install.sh
@@ -1,14 +1,4 @@
-#
-# TODO: This script is specific to debian/ubuntu. This should be cross-platform.
-#
-
-# install droonga
-apt-get update
-apt-get -y upgrade
-apt-get install -y ruby ruby-dev build-essential
-gem install droonga-engine --no-rdoc --no-ri
-
-SCRIPT_URL=https://raw.githubusercontent.com/droonga/droonga-engine/master/script/debian
+SCRIPT_URL=https://raw.githubusercontent.com/droonga/droonga-engine/master/script
 USER=droonga-engine
 DROONGA_BASE_DIR=/home/$USER/droonga
 
@@ -16,18 +6,57 @@ exist_user() {
   grep "^$1:" /etc/passwd > /dev/null
 }
 
-# add droonga-engine user and create files
-exist_user $USER || useradd -m $USER
+install_in_debian() {
+  # install droonga
+  apt-get update
+  apt-get -y upgrade
+  apt-get install -y ruby ruby-dev build-essential
+  gem install droonga-engine --no-rdoc --no-ri
 
-[ ! -e $DROONGA_BASE_DIR ] &&
-  mkdir $DROONGA_BASE_DIR
-[ ! -e $DROONGA_BASE_DIR/catalog.json ] &&
-  droonga-engine-catalog-generate --output=$DROONGA_BASE_DIR/catalog.json
-[ ! -e $DROONGA_BASE_DIR/droonga-engine.yaml ] &&
-  curl -o $DROONGA_BASE_DIR/droonga-engine.yaml $SCRIPT_URL/droonga-engine.yaml
-chown -R $USER.$USER $DROONGA_BASE_DIR
+  # add droonga-engine user and create files
+  exist_user $USER || useradd -m $USER
 
-# set up service
-[ ! -e /etc/init.d/droonga-engine ] &&
-  curl -o /etc/init.d/droonga-engine $SCRIPT_URL/droonga-engine
-update-rc.d droonga-engine defaults
+  [ ! -e $DROONGA_BASE_DIR ] &&
+    mkdir $DROONGA_BASE_DIR
+  [ ! -e $DROONGA_BASE_DIR/catalog.json ] &&
+    droonga-engine-catalog-generate --output=$DROONGA_BASE_DIR/catalog.json
+  [ ! -e $DROONGA_BASE_DIR/droonga-engine.yaml ] &&
+    curl -o $DROONGA_BASE_DIR/droonga-engine.yaml $SCRIPT_URL/debian/droonga-engine.yaml
+  chown -R $USER.$USER $DROONGA_BASE_DIR
+
+  # set up service
+  [ ! -e /etc/init.d/droonga-engine ] &&
+    curl -o /etc/init.d/droonga-engine $SCRIPT_URL/debian/droonga-engine
+  update-rc.d droonga-engine defaults
+}
+
+install_in_centos() {
+  yum update
+  yum -y groupinstall development
+  yum -y install ruby-devel npm
+  gem install droonga-engine --no-rdoc --no-ri
+
+  # add droonga-engine user and create files
+  exist_user $USER || useradd -m $USER
+
+  [ ! -e $DROONGA_BASE_DIR ] &&
+    mkdir $DROONGA_BASE_DIR
+  [ ! -e $DROONGA_BASE_DIR/catalog.json ] &&
+    droonga-engine-catalog-generate --output=$DROONGA_BASE_DIR/catalog.json
+  [ ! -e $DROONGA_BASE_DIR/droonga-engine.yaml ] &&
+    curl -o $DROONGA_BASE_DIR/droonga-engine.yaml $SCRIPT_URL/centos/droonga-engine.yaml
+  chown -R $USER.$USER $DROONGA_BASE_DIR
+
+  [ ! -e /etc/rc.d/init.d/droonga-engine ] &&
+    curl -o /etc/rc.d/init.d/droonga-engine $SCRIPT_URL/centos/droonga-engine
+  /sbin/chkconfig --add droonga-engine
+}
+
+if [ -e /etc/debian_version ] || [ -e /etc/debian_release ]; then
+  install_in_debian
+elif [ -e /etc/centos-release ]; then
+  install_in_centos
+else
+  echo "This supports only debian, CentOS."
+  return 255
+fi
