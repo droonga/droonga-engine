158d11c134da76beecaa83cf2c50be0c8c39e5c9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2014-09-07 23:42:31 +0900

    Use service's name for service scripts and configuration files

--- script/install.sh
+++ script/install.sh
@@ -36,7 +36,7 @@ setup_configuration_directory() {
   [ ! -e $DROONGA_BASE_DIR/catalog.json ] &&
     droonga-engine-catalog-generate --output=$DROONGA_BASE_DIR/catalog.json
   [ ! -e $DROONGA_BASE_DIR/$NAME.yaml ] &&
-    curl -o $DROONGA_BASE_DIR/droonga-engine.yaml $SCRIPT_URL/$PLATFORM/$NAME.yaml
+    curl -o $DROONGA_BASE_DIR/$NAME.yaml $SCRIPT_URL/$PLATFORM/$NAME.yaml
   chown -R $USER.$USER $DROONGA_BASE_DIR
 }
 
