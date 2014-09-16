2c4fb9da47d13dbe317820d32431731941f0ef37
YUKI Hiroshi <shimoda@clear-code.com>
2014-09-16 16:05:24 +0900

    Don't use sudo for CentOS

--- install.sh
+++ install.sh
@@ -14,12 +14,24 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 # Usage:
+#
+#  Ubuntu:
+#
 #   Install a release version:
-#     curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | sudo bash
+#     $ curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | sudo bash
 #   Install the latest revision from the repository:
-#     curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | sudo VERSION=master bash
+#     $ curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | sudo VERSION=master bash
 #   Install without prompt for the hostname:
-#     curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | sudo HOST=xxx.xxx.xxx.xxx bash
+#     $ curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | sudo HOST=xxx.xxx.xxx.xxx bash
+#
+#  CentOS 7:
+#
+#   Install a release version:
+#     # curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | bash
+#   Install the latest revision from the repository:
+#     # curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | VERSION=master bash
+#   Install without prompt for the hostname:
+#     # curl https://raw.githubusercontent.com/droonga/droonga-engine/master/install.sh | HOST=xxx.xxx.xxx.xxx bash
 
 NAME=droonga-engine
 SCRIPT_URL=https://raw.githubusercontent.com/droonga/$NAME/master/install
