d6ca2f74c260ca972cf86c9d180395cd9712adf9
YUKI Hiroshi <shimoda@clear-code.com>
2014-11-07 14:21:05 +0900

    Ignore undefined pid_file_path in the static configuration file

--- lib/droonga/command/droonga_engine.rb
+++ lib/droonga/command/droonga_engine.rb
@@ -128,7 +128,7 @@ module Droonga
           @ready_notify_fd = nil
 
           if have_config_file?
-            self.pid_file_path = config["pid_file"]
+            self.pid_file_path = config["pid_file"] if config["pid_file"]
             self.log_file  = config["log_file"] || Path.default_log_file
             self.log_level = config["log_level"] if config.include?("log_level")
           end
