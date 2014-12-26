4ce46fde9d6d8224a38396763490bc76b6caa23c
YUKI Hiroshi <shimoda@clear-code.com>
2014-12-26 15:42:13 +0900

    Load required module Path

--- lib/droonga/cluster_state.rb
+++ lib/droonga/cluster_state.rb
@@ -14,6 +14,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 require "droonga/loggable"
+require "droonga/path"
 require "droonga/file_observer"
 require "droonga/node_metadata"
 
