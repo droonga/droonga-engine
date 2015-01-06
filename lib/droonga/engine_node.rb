743702740308045aed843fccaadeea25a48e5d9f
YUKI Hiroshi <shimoda@clear-code.com>
2015-01-06 19:47:34 +0900

    Load required module

--- lib/droonga/engine_node.rb
+++ lib/droonga/engine_node.rb
@@ -13,7 +13,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "droonga/forwarder"
+require "droonga/loggable"
 require "droonga/fluent_message_sender"
 require "droonga/node_metadata"
 
