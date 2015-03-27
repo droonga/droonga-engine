91a1ac0507026a266f8082a67ecc8e4e966cc826
YUKI Hiroshi <shimoda@clear-code.com>
2015-03-27 17:19:00 +0900

    Revert "Remove too much require for same module"
    This reverts commit 1403be3fad554991b89d65d292186d4eff85ebc4.
    This is wrong way!

--- lib/droonga/engine.rb
+++ lib/droonga/engine.rb
@@ -24,6 +24,7 @@ require "droonga/engine_state"
 require "droonga/cluster"
 require "droonga/catalog_loader"
 require "droonga/dispatcher"
+require "droonga/file_observer"
 require "droonga/node_metadata"
 require "droonga/file_observer"
 
