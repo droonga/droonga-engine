885561d2e6c190695138f1983666f70f84eb3fa5
YUKI Hiroshi <shimoda@clear-code.com>
2015-03-27 15:08:02 +0900

    Bump copyright year

--- lib/droonga/logger.rb
+++ lib/droonga/logger.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2013-2014 Droonga Project
+# Copyright (C) 2013-2015 Droonga Project
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
