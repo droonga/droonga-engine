34b88a7abaf393d11b8846b117a62a28b16f3e24
YUKI Hiroshi <shimoda@clear-code.com>
2015-03-27 17:27:07 +0900

    Output debug log for operations around NodeMetadata

--- lib/droonga/node_metadata.rb
+++ lib/droonga/node_metadata.rb
@@ -14,11 +14,15 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 require "json"
+
+require "droonga/loggable"
 require "droonga/path"
 require "droonga/safe_file_writer"
 
 module Droonga
   class NodeMetadata
+    include Loggable
+
     module Role
       SERVICE_PROVIDER   = "service-provider"
       ABSORB_SOURCE      = "absorb-source"
@@ -51,12 +55,14 @@ module Droonga
     end
 
     def set(key, value)
+      logger.debug("setting: #{key}=#{new_role}")
       key = normalize_key(key)
       @metadata[key] = value
       SafeFileWriter.write(metadata_file, JSON.pretty_generate(@metadata))
     end
 
     def delete(key)
+      logger.debug("deleting: #{key}")
       key = normalize_key(key)
       @metadata.delete(key)
       SafeFileWriter.write(metadata_file, JSON.pretty_generate(@metadata))
@@ -91,6 +97,7 @@ module Droonga
     def load
       if metadata_file.exist?
         contents = metadata_file.read
+        logger.debug("metadata loaded from file", :metadata => contents)
         unless contents.empty?
           return JSON.parse(contents, :symbolize_names => true)
         end
