b3b9da54cc5ba4655585cd7950f9fcf749b90e82
YUKI Hiroshi <shimoda@clear-code.com>
2015-03-27 17:12:57 +0900

    Load required module correctly

--- lib/droonga/engine_node.rb
+++ lib/droonga/engine_node.rb
@@ -17,6 +17,7 @@ require "droonga/loggable"
 require "droonga/forward_buffer"
 require "droonga/fluent_message_sender"
 require "droonga/node_metadata"
+require "droonga/file_observer"
 
 module Droonga
   class EngineNode
