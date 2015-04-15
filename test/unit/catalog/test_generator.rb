412ca73e4feefd273cee217c53d7607d4b337214
YUKI Hiroshi <shimoda@clear-code.com>
2015-04-15 14:39:36 +0900

    Follow to change of class relations

--- test/unit/catalog/test_generator.rb
+++ test/unit/catalog/test_generator.rb
@@ -13,11 +13,11 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "droonga/catalog_generator"
+require "droonga/catalog/generator"
 
 class CatalogGeneratorTest < Test::Unit::TestCase
   def setup
-    @generator = Droonga::CatalogGenerator.new
+    @generator = Droonga::Catalog::Generator.new
     @normalized_time_value = "2014-02-09T00:00:00Z"
   end
 
