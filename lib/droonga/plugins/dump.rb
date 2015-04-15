f4779f5805a475a2f73e6dbc0c0a77369d92545c
YUKI Hiroshi <shimoda@clear-code.com>
2015-04-15 21:20:03 +0900

    Reduce annoying trace logs around watchers

--- lib/droonga/plugins/dump.rb
+++ lib/droonga/plugins/dump.rb
@@ -67,15 +67,15 @@ module Droonga
                 runner.resume
               rescue
                 timer.detach
-                logger.trace("start: watcher detached on unexpected exception",
-                             :watcher => timer)
+                # logger.trace("start: watcher detached on unexpected exception",
+                #              :watcher => timer)
                 logger.exception(error_message, $!)
                 error(error_name, error_message)
               end
             else
               timer.detach
-              logger.trace("start: watcher detached on unexpected exception",
-                           :watcher => timer)
+              # logger.trace("start: watcher detached on unexpected exception",
+              #              :watcher => timer)
             end
           end
 
