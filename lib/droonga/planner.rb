ada4b9468c7a6a464e9baf641f9431c1682567f3
YUKI Hiroshi <shimoda@clear-code.com>
2015-04-17 15:45:29 +0900

    Don't reduce results if the "replyTo" is not given, for now

--- lib/droonga/planner.rb
+++ lib/droonga/planner.rb
@@ -35,7 +35,23 @@ module Droonga
       options = {
         :record => params[:record],
       }
-      if @collector_class
+
+      #TODO: We don't have to reduce results of the message when
+      #      the message doesn't have "replyTo" information, because:
+      #
+      #       * Currently the "super step" mecahnism is not
+      #         implemented yet.
+      #       * So, reduced results won't be forwarded to other
+      #         handlers directly. Results will be forwarded to
+      #         the sender as the "response".
+      #       * So, if "replyTo" information is not given, the
+      #         reduced result will have no receiver.
+      #
+      #      However, in the future after the "super step" mechanism
+      #      is introduced, reduced results can be required even if
+      #      the request message have no "replyTo" information.
+      #      Then we must update this logic.
+      if @collector_class and message["replyTo"]
         reduce_key = "result"
         options[:reduce] = {
           reduce_key => @collector_class.operator,
@@ -70,7 +86,7 @@ module Droonga
         scatter_options[:replica] = "random"
       end
       planner.scatter(scatter_options)
-      planner.reduce(options[:reduce])
+      planner.reduce(options[:reduce]) if options[:reduce]
       planner.plan
     end
 
@@ -86,7 +102,7 @@ module Droonga
         broadcast_options[:replica] = "random"
       end
       planner.broadcast(broadcast_options)
-      planner.reduce(options[:reduce])
+      planner.reduce(options[:reduce]) if options[:reduce]
       planner.plan
     end
   end
