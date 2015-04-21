f25daf7ec10d10f578058c0d2855ad0943c14736
YUKI Hiroshi <shimoda@clear-code.com>
2015-04-21 22:00:04 +0900

    Move "require" to correct requirere

--- lib/droonga/plugins/dump.rb
+++ lib/droonga/plugins/dump.rb
@@ -13,6 +13,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+require "fiber"
 require "groonga"
 
 require "droonga/plugin"
