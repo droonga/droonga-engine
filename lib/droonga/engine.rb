5fc962aabcfa3659c2711e4e62e89858f1187df7
YUKI Hiroshi <shimoda@clear-code.com>
2015-04-23 13:56:29 +0900

    Load required module

--- lib/droonga/engine.rb
+++ lib/droonga/engine.rb
@@ -16,6 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 require "fileutils"
+require "time"
 
 require "droonga/engine/version"
 require "droonga/loggable"
