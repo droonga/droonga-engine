9daf927645d7dedfbabece7c86dbce27d443b581
YUKI Hiroshi <shimoda@clear-code.com>
2015-04-30 16:23:56 +0900

    Add more tests for ForwardBuffer

--- test/unit/test_forward_buffer.rb
+++ test/unit/test_forward_buffer.rb
@@ -13,6 +13,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+require "time"
+
 require "droonga/forward_buffer"
 
 class ForwardBufferTest < Test::Unit::TestCase
@@ -22,9 +24,16 @@ class ForwardBufferTest < Test::Unit::TestCase
 
   def setup
     setup_temporary_directory
+
     @buffer = ForwardBuffer.new("node29:2929/droonga")
     @buffer.data_directory = @temporary_directory
     @buffer.serf = StubSerf.new
+
+    @forwarded_messages = []
+    @buffer.on_forward = lambda do |message, destination|
+      @forwarded_messages << {:message => message,
+                              :destination => destination}
+    end
   end
 
   def teardown
@@ -38,5 +47,60 @@ class ForwardBufferTest < Test::Unit::TestCase
   def test_not_empty
     @buffer.add({}, {})
     assert_false(@buffer.empty?)
+  end
+
+  def test_add
+    @buffer.add({}, {})
+    @buffer.add({}, {})
+    @buffer.add({}, {})
+    assert_equal(3, @buffer.buffered_messages.size)
+  end
+
+  class ForwardTest < self
+    def setup
+      super
+      @buffer.add({"date" => "2015-04-30T00:00:00.000000Z"}, {})
+      @buffer.add({"date" => "2015-04-30T01:00:00.000000Z"}, {})
+      @buffer.add({"date" => "2015-04-30T02:00:00.000000Z"}, {})
+    end
+
+    def test_without_boundary
+      @buffer.start_forward
+      assert_equal({
+                     :empty => true,
+                     :forwarded => [
+                       {:message => {"date" => "2015-04-30T00:00:00.000000Z",
+                                     "xSender"=>"forward-buffer"},
+                        :destination => {}},
+                       {:message => {"date" => "2015-04-30T01:00:00.000000Z",
+                                     "xSender"=>"forward-buffer"},
+                        :destination => {}},
+                       {:message => {"date" => "2015-04-30T02:00:00.000000Z",
+                                     "xSender"=>"forward-buffer"},
+                        :destination => {}},
+                     ],
+                   },
+                   {
+                     :empty => @buffer.empty?,
+                     :forwarded => @forwarded_messages,
+                   })
+    end
+
+    def test_with_boundary
+      @buffer.process_messages_newer_than(Time.parse("2015-04-30T01:00:00.000000Z"))
+      @buffer.start_forward
+      assert_equal({
+                     :empty => true,
+                     :forwarded => [
+                       {:message => {"date" => "2015-04-30T02:00:00.000000Z",
+                                     "xSender"=>"forward-buffer"},
+                        :destination => {}},
+                     ],
+                   },
+                   {
+                     :empty => @buffer.empty?,
+                     :forwarded => @forwarded_messages,
+                   })
+    end
   end
 end
