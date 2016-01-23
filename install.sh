5753632751a397fddfad8c1ade58af5cc2afedaf
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-01-23 18:48:55 +0900

    Remove installation of unneccessary script functions.sh

--- install.sh
+++ install.sh
@@ -354,11 +354,6 @@ install() {
     exit 1
   fi
 
-  curl -s -o $TEMPDIR/functions.sh $(download_url "install/$PLATFORM/functions.sh")
-  if ! source $TEMPDIR/functions.sh; then
-    echo "ERROR: Failed to download post-installation script!"
-    exit 1
-  fi
   if ! exist_command register_service; then
     echo "ERROR: Downloaded post-installation script is broken!"
     exit 1
